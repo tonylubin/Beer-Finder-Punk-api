e687b410eeff3c2f45bb65e631b5741fc30861b0
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-08-24 12:40:46 +0100

    added search box

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import image from "../../../src/BrewDog-logo.jpg";
+import SearchBox from "../SearchBox/SearchBox";
 
-const Header = () => {
+const Header = ({ setSearchItem }) => {
   return (
     <header className={styles.header}>
       <div className={styles.header__imgContainer}>
@@ -11,6 +12,9 @@ const Header = () => {
       <div className={styles.header__heading}>
         <h1>BrewDog</h1>
         <p>" A Beer Finder for searching for a BrewDog beer that quenches that cold beer thirst..."</p>
+      </div>
+      <div className={styles.header__searchbox}>
+        <SearchBox setSearchItem={setSearchItem}/>
       </div>
     </header>
   );
